3c9e3c900dd37dd2bb6d7935c9cf4c69bd80da9a
zoy2control <paulino_zoy_it@163.com>
2018-11-15 20:59:48 +0800

    BasicGrammar beta0.1

--- src/test/java/rt/java/basic/BasicGrammar.java
+++ src/test/java/rt/java/basic/BasicGrammar.java
@@ -12,7 +12,7 @@ public class BasicGrammar {
      * ·逻辑运算符 ||与 三目运算符的顺序
      */
     @Test
-    public void logicOperator() {
+    public void logicOperatorTest() {
         int i = compareTo(Integer.class, 1, 2);// ·eq1
 //        int i = compareTo(Integer.class, 10, 2);// ·eq2
 //        int i = compareTo(Integer.class, 2, 2);// ·eq3
@@ -28,5 +28,14 @@ public class BasicGrammar {
             i = ((Comparable)k).compareTo(x);// ·比较 k、x的大小
         }
         return i;
+    }
+
+   @Test
+    public void shiftOperatorTest() {
+        int oldCapacity = 10;
+        int newCapacity = oldCapacity << 1;// ·右移1位：*2
+
+        System.out.println("oldCap:" + oldCapacity);// ·注意，oldCap值不变
+        System.out.println("newCap:" + newCapacity);
     }
 }
---
+++ src/test/java/rt/java/util/HashMap/HashMapTest.java
@@ -0,0 +1,7 @@
+package rt.java.util.HashMap;
+
+/**
+ * Created by zouzp on 2018/11/15.
+ */
+public class HashMapTest {
+}
