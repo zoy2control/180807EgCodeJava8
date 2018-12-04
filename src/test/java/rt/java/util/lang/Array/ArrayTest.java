7fa9b962e4bf8f4a89f44d9b5ae9e5bb6f7ed472
zoy2control <paulino_zoy_it@163.com>
2018-12-04 18:13:16 +0800

    Integer beta0.1

---
+++ src/test/java/rt/java/util/lang/Array/ArrayTest.java
@@ -0,0 +1,41 @@
+package rt.java.util.lang.Array;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/**
+ * Created by zouzp on 2018/12/3.
+ */
+public class ArrayTest {
+    @Test
+    public void test01() {
+        /**
+         *  ·数组初始化
+         */
+//        char[] chars = new char[];// ·初始化数组必须明确指定 长度
+        char[] a = new char[10];// ·eq1
+        char[] b = {'1', '2'};// ·eq2
+
+
+        /**
+         * ·可不指定长度，用 集合
+         */
+//        ArrayList<int> ints = new ArrayList<int>;// ·但集合的泛型不可以是 int/char/long等自带类型，而是要对象
+        ArrayList<Integer> integerArrayList = new ArrayList<>();
+    }
+
+    /**
+     * ·数组和 String之间转换
+     */
+    @Test
+    public void testConvertArrayBtwString() {
+        /* ·char[]转 String*/
+        char[] c = new char[10];
+        String s = new String(c);// ·eq1
+        String s1 = String.valueOf(c);// ·eq2
+
+        /* ·String转 char[]*/
+        char[] stringChars = s.toCharArray();
+    }
+}
--- src/test/java/rt/java/util/lang/Integer/IntegerTest.java
+++ src/test/java/rt/java/util/lang/Integer/IntegerTest.java
@@ -6,6 +6,10 @@ import org.junit.Test;
  * Created by zouzp on 2018/11/26.
  */
 public class IntegerTest {
+
+    /**
+     * ·对象引用的 ==
+     */
     @Test
     public void test01() {
         Integer a = 99, b = 99;
@@ -14,6 +18,13 @@ public class IntegerTest {
         System.out.println("a == b结果是：" + Boolean.toString(a == b));
         System.out.println("c == d结果是：" + Boolean.toString(c == d));
 
-        // ·-XX:AutoBoxCacheMax=1000，可以扩大 自动装箱、拆箱范围
+        // ·虚拟机选项添加 -XX:AutoBoxCacheMax=1000，可以扩大 自动装箱、拆箱范围
+    }
+
+    @Test
+    public void test02() {
+        Integer a = new Integer(99);
+        Integer b = new Integer(99);
+        System.out.println("a == b结果是：" + Boolean.toString(a == b));// ·注意 a,b是引用，而不是 实际值
     }
 }
