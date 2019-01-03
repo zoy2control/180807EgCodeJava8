a99dc4b67563e5b8aac56818b6261f7f83d6c515
zoy2control <paulino_zoy_it@163.com>
2019-01-03 17:57:04 +0800

    InputAndOutput beta0.1

--- src/test/java/zoy2control/cp1_fundamentalStructure/sec3_operators/InputAndOutput.java
+++ src/test/java/zoy2control/cp1_fundamentalStructure/sec3_operators/InputAndOutput.java
@@ -11,6 +11,19 @@ public class InputAndOutput {
     @Test
     public void scannerEg() {
         /* ·对于不是 java.lang下的 java文件，引用的时候需要 import显示导入*/
-//        Scanner scanner = new Scanner();
+        /* ·GUI输入输出区别 控制台输入输出*/
+
+        /* ·标准输入流 System.in */
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("what's your name");
+        String name = scanner.nextLine();
+
+        System.out.print("how old are you");
+        int age = scanner.nextInt();
+
+        System.out.print(name + ":" + age);
     }
 }
